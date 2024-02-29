1952c7cfdb57f25f99061795705aab7919eae981
doankhiem <khiembi161219@gmail.com>
2024-02-29 22:30:01 +0700

    use nuxt link

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,19 @@
 <template>
-    <div>
-      
-      hihihihi
+    <div id="root">
+        <header>
+            <nav>
+                <ul>
+                    <li>
+                        <NuxtLink to="/">Home</NuxtLink>
+                    </li>
+                    <li>
+                        <NuxtLink to="/decks">Decks</NuxtLink>
+                    </li>
+                    <li>
+                        <NuxtLink to="/profile">Profile</NuxtLink>
+                    </li>
+                </ul>
+            </nav>
+        </header>
     </div>
   </template>
