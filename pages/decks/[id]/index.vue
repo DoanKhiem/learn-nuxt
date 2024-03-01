041cdfd71258544ba36aaf4f01e75a52717de149
doankhiem <khiembi161219@gmail.com>
2024-03-01 22:55:19 +0700

    Route Validation

--- pages/decks/[id]/index.vue
+++ pages/decks/[id]/index.vue
@@ -3,3 +3,12 @@
         Your desk id is: {{ $route.params.id }}
     </h1>
 </template>
+
+<script setup>
+definePageMeta({
+    validate: async (route) => {
+        // Check if the id is made up of digits
+        return /^[0-9]{9,12}$/.test(route.params.id)
+    }
+})
+</script>
