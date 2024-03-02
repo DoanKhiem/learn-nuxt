7e39e3ec668a7825afcc7f632861a2c6422889d5
doankhiem <khiembi161219@gmail.com>
2024-03-02 23:52:52 +0700

    multiple layouts

--- app.vue
+++ app.vue
@@ -1,5 +1,14 @@
 <template>
   <div>
-    <NuxtPage />
+    <!-- <NuxtPage /> -->
+    <NuxtLayout :name="layout">
+      
+  </NuxtLayout>
+  <NuxtPage />
   </div>
+  
 </template>
+<script setup>
+// You might choose this based on an API call or logged-in status
+const layout = "public";
+</script>
