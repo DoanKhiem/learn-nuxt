fcf5bd76ada16debf67d004e34afb2c04dc1b75e
doankhiem <khiembi161219@gmail.com>
2024-03-02 23:37:13 +0700

    show children component

---
+++ pages/settings/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <main class="main">
+        <h3>General settings...</h3>
+    </main>
+</template>
+
+<style >
+
+    .main {
+        margin-left: 3rem;
+    }
+</style>
---
+++ pages/settings/schedule/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <main class="main">
+        <h3>Schedule settings...</h3>
+    </main>
+</template>
+
+<style>
+
+
+    .main {
+        margin-left: 3rem;
+    }
+</style>
