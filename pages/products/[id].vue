18a7681a62cc5b2d39e1afd9ac6038c19b28af25
khiemdev <khiembi161219@gmail.com>
2024-03-05 13:53:43 +0700

    Reusable Components

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,8 +1,7 @@
 <template>
   <div>
-    <p>{{ product.title }}</p>
-    <p>{{ product.price }}</p>
-    <p>{{ product.id }}</p>
+    
+    <ProductDetails :product="product" />
   </div>
 </template>
 
