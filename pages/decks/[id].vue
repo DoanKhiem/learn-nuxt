ac02ae6451d4e8b92585f5b992b273434ca52d28
khiemdev <khiembi161219@gmail.com>
2024-05-06 10:27:15 +0700

    nuxt link

---
+++ pages/decks/[id].vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>Deck {{ id }}</h1>
+    </div>
+</template>
+
+<script setup lang="ts">
+const { id } = useRoute().params;
+</script>
+
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,20 @@
 <template>
-    <div>
+    <div id="root">
+        <header>
+            <nav>
+                <ul>
+                    <li>
+                        <nuxt-link to="/">Home</nuxt-link>
+                    </li>
+                    <li>
+                        <nuxt-link to="/decks">Decks</nuxt-link>
+                    </li>
+                    <li>
+                        <nuxt-link to="/profile">Profile</nuxt-link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
         <h1>Home</h1>
     </div>
 </template>
