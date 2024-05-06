1ea51fa7771d576c7429d897c1e586600b0f5e6a
khiemdev <khiembi161219@gmail.com>
2024-05-06 13:23:13 +0700

    page error

---
+++ error.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import type { NuxtError } from '#app'
+
+const props = defineProps({
+  error: Object as () => NuxtError
+})
+</script>
+
+<template>
+  <div>
+    <h2>hihiihihi</h2>
+    <h1>{{ error && error.statusCode }}</h1>
+    <NuxtLink to="/">Go back home</NuxtLink>
+  </div>
+</template>
