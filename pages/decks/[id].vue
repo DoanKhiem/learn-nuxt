8f8b697dec96e1b79a4c2245c65dff6013b71972
khiemdev <khiembi161219@gmail.com>
2024-05-06 10:38:33 +0700

    Route Validation

--- pages/decks/[id].vue
+++ pages/decks/[id].vue
@@ -6,6 +6,12 @@
 
 <script setup lang="ts">
 const { id } = useRoute().params;
+definePageMeta({
+  validate: async ({ params }) => {
+    // Check if the id is made up of digits
+    return typeof params.id === 'string' && /^\d+$/.test(params.id)
+  }
+})
 </script>
 
 <style scoped>
