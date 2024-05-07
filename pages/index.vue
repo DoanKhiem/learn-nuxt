c0eb96fd3a6fd7e537c106c61ba9e555919325c6
khiemdev <khiembi161219@gmail.com>
2024-05-07 09:34:07 +0700

    style for component

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,10 @@
 <template>
     <div id="root">
-        <header>
-            <nav>
-                <ul>
-                    <li>
-                        <nuxt-link to="/">Home</nuxt-link>
-                    </li>
-                    <li>
-                        <nuxt-link to="/decks">Decks</nuxt-link>
-                    </li>
-                    <li>
-                        <nuxt-link to="/profile">Profile</nuxt-link>
-                    </li>
-                </ul>
-            </nav>
-        </header>
         <h1>Home</h1>
     </div>
 </template>
 
 <script setup lang="ts">
-
 </script>
 
 <style scoped>
