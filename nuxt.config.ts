49345fa43722e0f837307a015ee974e2495b227c
khiemdev <khiembi161219@gmail.com>
2024-05-07 10:18:18 +0700

    add font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: ['~/assets/css/main.min.css'],
+  app: {
+    head: {
+      link: [{ rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Poetsen+One&display=swap' }]
+    }
+  },
+  css: ['~/assets/css/main.min.css', '~/assets/css/external.css'],
 })
