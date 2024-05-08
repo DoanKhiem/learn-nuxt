a4acef8bdda0ebe9ae79f75b269b4846575a84fc
khiemdev <khiembi161219@gmail.com>
2024-05-08 10:10:43 +0700

    page home

---
+++ components/Footer/DefaultFooter.vue
@@ -0,0 +1,17 @@
+<template>
+    <footer class="text-center">
+        <hr class="my-5">
+        <p>
+            Design by <a href="https://www.facebook.com/doankhiem1999">Đoàn Khiêm</a>
+        </p>
+        <p>2024</p>
+    </footer>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -3,10 +3,12 @@
       <DefaultHeader />
     <!-- <p>Some default layout content shared across all pages</p> -->
     <slot />
+    <DefaultFooter />
   </div>
 </template>
 
 <script setup lang="ts">
+import DefaultFooter from '~/components/Footer/DefaultFooter.vue';
 import DefaultHeader from '~/components/Header/DefaultHeader.vue';
 </script>
 
